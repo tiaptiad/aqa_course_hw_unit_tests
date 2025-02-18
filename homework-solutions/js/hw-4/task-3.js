c282891640bc88b603a5c6cb709d2c6766ce63d7
Dmytro <79286680+tiaptiad@users.noreply.github.com>
2025-02-18 22:17:43 +0100

    HW4 Solutions

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -1,8 +1,19 @@
-/*
+let minAge = 18;
+let maxAge = 60;
+let age="20";
 
-(НЕ ОБЯЗАТЕЛЬНО)
+age = Number(age);
 
-Преобразовать Task 2 таким образом, чтобы значение НАПРИМЕР '2' (т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось, 
-  преобразовываясь в number
-
-*/
+if(typeof age == 'number'){
+  if(age<minAge){
+    console.log("You don't have access cause your age is " + age + " It's less then " + minAge);
+  } else if (age>=minAge && age<maxAge){
+    console.log('Welcome  !');
+  } else if(age>maxAge){
+    console.log('Keep calm and look Culture channel');
+  } else {
+    console.log('Technical work');
+  }
+} else {
+  console.log('Incorrect data type');
+};
